51f0d50e1e610266d7f225101d7779d41c6f1ef6
rajankumarrkr <rajankumarrkr4576@gmail.com>
2026-01-26 18:14:45 +0530

    Fix width of BottomNav

--- frontend/src/components/Layout/BottomNav.jsx
+++ frontend/src/components/Layout/BottomNav.jsx
@@ -11,15 +11,15 @@ const BottomNav = () => {
   ];
 
   return (
-    <div className="fixed bottom-6 left-0 right-0 z-50 px-6">
-      <nav className="max-w-md mx-auto glass-card rounded-2xl overflow-hidden border border-white/40 dark:border-gray-700/50 shadow-2xl">
-        <div className="flex justify-around items-center h-16 px-2">
+    <div className="fixed bottom-6 left-0 right-0 z-50 px-4">
+      <nav className="max-w-[440px] mx-auto glass-card rounded-2xl overflow-hidden border border-white/40 dark:border-gray-700/50 shadow-2xl">
+        <div className="flex justify-around items-center h-16 ">
           {navItems.map((item) => (
             <NavLink
               key={item.path}
               to={item.path}
               className={({ isActive }) =>
-                `flex flex-col items-center justify-center flex-1 py-1 transition-all duration-300 relative ${isActive
+                `flex flex-col items-center justify-center flex-1 py-3  transition-all duration-300 relative ${isActive
                   ? 'text-saffron-600 dark:text-saffron-500'
                   : 'text-gray-500 dark:text-gray-400 hover:text-saffron-400'
                 }`
